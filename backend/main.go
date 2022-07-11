b016ce2c407f2ec41e0902610f548ea5d597a060
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-07-11 23:31:32 +0300

    Basic server setup with health endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func healthHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, "Server is healthy.")
+}
 
 func main() {
 	fmt.Println("Hello project Corporate Mundo")
+
+	router := gin.Default()
+	router.GET("/health", healthHandler)
+
+	router.Run(":8030")
 }
